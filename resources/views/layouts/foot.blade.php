f7b42d533062dcbb57b3b13dae0427125e33f0da
maproderm <maprodermweb@gmail.com>
2022-11-12 11:32:28 -0600

    first commit

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,19 @@
+<!-- Vendor -->
+    <!--begin::Javascript-->
+		<script>var hostUrl = "assets/";</script>
+		<!--begin::Global Javascript Bundle(mandatory for all pages)-->
+		<script src="metronic/assets/plugins/global/plugins.bundle.js"></script>
+		<script src="metronic/assets/js/scripts.bundle.js"></script>
+		<!--end::Global Javascript Bundle-->
+		<!--begin::Vendors Javascript(used for this page only)-->
+		<script src="metronic/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
+		<script src="metronic/assets/plugins/custom/typedjs/typedjs.bundle.js"></script>
+		<!--end::Vendors Javascript-->
+		<!--begin::Custom Javascript(used for this page only)-->
+		<script src="metronic/assets/js/custom/landing.js"></script>
+		<script src="metronic/assets/js/custom/pages/pricing/general.js"></script>
+		<!--end::Custom Javascript-->
+		<!--end::Javascript-->
+
+    </body>
+</html>
